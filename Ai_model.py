f32758481878a097e08bd6cc0398358750177037
Chetan kharade <chetankharade09@gamil.com>
2025-02-22 19:36:09 +0530

    u

--- Ai_model.py
+++ Ai_model.py
@@ -1,8 +1,8 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-# from tensorflow.keras.models import Sequential
-# from tensorflow.keras.layers import Dense
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
 
 
 csv_file = "dataa.csv" 
